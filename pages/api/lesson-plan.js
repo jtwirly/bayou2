a8ed78a26e025a23cb88c0d697486ae84250b431
Jennifer Turliuk <jenn@makerkids.com>
2023-03-26 00:52:54 -0400

    reduced tokens

--- pages/api/lesson-plan.js
+++ pages/api/lesson-plan.js
@@ -23,7 +23,7 @@ const getLessonPlan = async (req, res) => {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `Create a lesson plan for a ${gradeLevel} grade ${subject} class with a duration of ${duration}.`,
-      max_tokens: 3500,
+      max_tokens: 1024,
       temperature: 0.8
     });
 
