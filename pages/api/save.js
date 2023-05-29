fc5c513727b6fde11df93e15571fef28a00b5510
Jennifer Turliuk <jenn@makerkids.com>
2023-05-29 17:24:56 -0400

    Updated prompt engineering

--- pages/api/save.js
+++ pages/api/save.js
@@ -65,25 +65,25 @@ const saveToSupabase = async (req, res) => {
       url: `/${nanoid()}`,
     };
 
-    console.log('dataToInsert ', dataToInsert)
+    //console.log('dataToInsert ', dataToInsert)
      const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Create a lesson plan for a ${gradeLevel} grade ${subject} class following the ${curriculum} curriculum, focusing on the strand of ${strand}, the topic of ${topic} and the expectations of ${expectations}, with a duration of ${duration}, using the ${method} pedagogical method, the ${framework} framework, and the ${mode} learning mode, taking into account considerations for ${considerations} and accommodation for ${accommodations}. Ensure there is differentiation and leveled activities as part of the lesson plan.`,
+      prompt: `Create a lesson plan for a grade ${gradeLevel} ${subject} class following the ${curriculum} curriculum, focusing on the strand of ${strand}, the topic of ${topic} and the expectations of ${expectations}, with a duration of ${duration}, using the ${method} pedagogical method, the ${framework} framework, and the ${mode} learning mode, taking into account considerations for ${considerations} and accommodation for ${accommodations}. Ensure there is differentiation and leveled activities as part of the lesson plan.`,
       max_tokens: 1024,
       temperature: 0.8
     });
     console.log('completion', completion.data);
 
     const text = completion.data.choices[0].text.trim();
-    console.log('text', text)
+    ////console.log('text', text)
     const cleanedText = text.replace(/^[.,\s]+|[.,\s]+$/g, "");
-    console.log('cleaned text: ', cleanedText);
+    //console.log('cleaned text: ', cleanedText);
     dataToInsert.lessonplan = cleanedText;
   const {data} = await supabaseClient
       .from('lessonplans')
       .insert(dataToInsert).select();
 
-console.log('data in save', data);
+//console.log('data in save', data);
 
    // if (error) {
    //   throw error;
@@ -92,7 +92,7 @@ console.log('data in save', data);
     res.status(200).json({data});
     console.log('returning')
   } catch (error) {
-    console.error("Error in saveToSupabase:", error);
+    console.error("Error in saveToSupabase:", error.response, error.message);
     res.status(500).json({ error: error.message });
   }
 };
